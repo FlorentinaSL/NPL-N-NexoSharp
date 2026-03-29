b25a35d712d0cbdebfcfa8ea2f4e0fd1486e9725
MRCAT9898 <luca.cisternino.carovigno@gmail.com>
2026-03-29 16:34:36 +0200

    release: Finalize v3.0 stable binary and installer rebuild

--- NexoCompiler/obj/Release/net9.0/win-x64/NexoCompiler.AssemblyInfo.cs
+++ NexoCompiler/obj/Release/net9.0/win-x64/NexoCompiler.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Nexo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95efdf9db4a2dac5d86beeb05d0fb28f43406039")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05bf71b109cc49645c9fe9fb71290f1eb26ddaa5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Nexo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Nexo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
